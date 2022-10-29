6223be35b2c099c351cd329f055aaefd2f42ab28
Anatolii Horobets <admin@cheapsite.studio>
2022-10-29 14:00:46 +0300

    correct settings

--- day-01.js
+++
@@ -1,7 +0,0 @@
-/**
-https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
-*/
-function correctTail(body, tail) {
-  const sub = body.substr(body.length - tail.length);
-  if (sub === tail) return true; return false;
-}
